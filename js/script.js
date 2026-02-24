e99a6640e3fef88645e7c3d32ee1f11f77c247c3
Sali Janjalashvili <salijanjalashvili@gmail.com>
2026-02-25 02:49:13 +0400

    Add mobile nav

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 document.querySelector('.theme-toggle').addEventListener('click', () => {
-  document.querySelector('body').classList.toggle('dark');
+  document.body.classList.toggle('dark');
+  document.querySelector('.site-header').classList.toggle('dark');
+});
+
+document.querySelector('.burger-menu').addEventListener('click', () => {
+  document.body.classList.toggle('menu-open');
+  document.querySelector('.theme-toggle').classList.toggle('display');
+  document.querySelector('.burger-menu').classList.toggle('display');
+  document.querySelector('.mobile-nav').classList.toggle('display');
 });
